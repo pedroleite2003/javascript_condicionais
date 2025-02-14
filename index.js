3c8e7cad5f5236a3c156ad100b681640f867a146
pedroleite2003 <pedromilk2003@gmail.com>
2025-02-14 12:03:32 -0300

    estrutura de condição

---
+++ index.js
@@ -0,0 +1,123 @@
+let porta = false;
+
+if (porta){
+    console.log("sair");
+} else{
+    console.log("abra a porta");
+}
+
+let semaforo = "verde";
+
+if  (semaforo == "verde"){
+    console.log("siga em frete");
+} else if (semaforo == "amarelo"){
+    console.log("acelere");
+} else{
+    console.log("pare");
+}
+
+if (2 === "2" || 2>1){
+    console.log("sim");
+}
+
+if (2 ==="2" && 2>1){
+    console.log("sim");
+} else{
+    console.log("nao");
+}
+
+
+let mes = "Fevereiro";
+let dia = 8;
+let nomedeForrozeiro = "";
+
+if (mes == "Janeiro"){
+    nomedeForrozeiro = "Forrozeiro";
+} else if(mes == "Fevereiro"){
+    nomedeForrozeiro = "Avexado (a)";
+} else if(mes == "Março"){
+    nomedeForrozeiro = "Arretado (a)";
+} else if(mes == "Abril"){
+    nomedeForrozeiro = "Caipira";
+} else if(mes == "Maio"){
+    nomedeForrozeiro = "Flor";
+} else if(mes == "Junho"){
+    nomedeForrozeiro = "Rei/Rainha";
+} else if(mes == "Julho"){
+    nomedeForrozeiro = "Sanfoneiro (a)";
+} else if(mes == "Agosto"){
+    nomedeForrozeiro = "Fazendeiro (a)";
+} else if(mes == "Setembro"){
+    nomedeForrozeiro = "Pipoqueiro (a)";
+} else if(mes == "Outubro"){
+    nomedeForrozeiro = "Casamenteiro (a)";
+} else if(mes == "Novembro"){
+    nomedeForrozeiro = "Pixotinho (a)";
+} else{
+    nomedeForrozeiro = "Marrento (a);"
+}
+
+
+if(dia == 1){
+    nomedeForrozeiro += " Do Milho";
+} else if(dia == 2){
+    nomedeForrozeiro += " Da Quadrilha";
+} else if(dia == 3){
+    nomedeForrozeiro += " Da Bexiga";
+} else if(dia == 4){
+    nomedeForrozeiro += " Do Amor";
+} else if(dia == 5){
+    nomedeForrozeiro += "  Junino (a)";
+} else if(dia == 6){
+    nomedeForrozeiro += " Do São João";
+} else if(dia == 7){
+    nomedeForrozeiro += " Do Forro";
+} else if(dia == 8){
+    nomedeForrozeiro += " Do Arraiá";
+} else if(dia == 9){
+    nomedeForrozeiro += " Do Vale do São Francisco";
+} else if(dia == 10){
+    nomedeForrozeiro += " Da Gota Serena";
+} else if(dia == 11){
+    nomedeForrozeiro += " Casamenteiro (a)";
+} else if(dia == 12){
+    nomedeForrozeiro += " Do Agreste";
+} else if(dia == 13){
+    nomedeForrozeiro += " Da Canjica";
+} else if(dia == 14){
+    nomedeForrozeiro += " Do Vinho Quente";
+} else if(dia == 15){
+    nomedeForrozeiro += " Do Amendoim";
+} else if(dia == 16){
+    nomedeForrozeiro += " Da Pescaria";
+} else if(dia == 17){
+    nomedeForrozeiro += " Do Chapéu";
+} else if(dia == 18){
+    nomedeForrozeiro += " Da Foguira";
+} else if(dia == 19){
+    nomedeForrozeiro += " Da Barrac do Beijo";
+} else if(dia == 20){
+    nomedeForrozeiro += " D curau";
+} else if(dia == 21){
+    nomedeForrozeiro += " Da Prenda";
+} else if(dia == 22){
+    nomedeForrozeiro += " Da Grande Roda";
+} else if(dia == 23){
+    nomedeForrozeiro += " Do Túnel";
+} else if(dia == 24){
+    nomedeForrozeiro += " Da Saia Roodada";
+} else if(dia == 25){
+    nomedeForrozeiro += " Da Camisa Quadanculada";
+} else if(dia == 26){
+    nomedeForrozeiro += " Da Maça do Amor";
+} else if(dia == 27){
+    nomedeForrozeiro += " Do Violão";
+} else if(dia == 28){
+    nomedeForrozeiro += " Da Baião";
+} else if(dia == 29 && mes ){
+    nomedeForrozeiro += " Do Quentão";
+} else if(dia == 30){
+    nomedeForrozeiro += " Da Bandeirinha";
+} else{
+    nomedeForrozeiro += " do Vinho Quente"
+}
